e227ce03a813c3040fe8716b8e1fe7ed60aeb06a
Melody <reticivis@gmail.com>
2019-11-01 21:08:23 -0500

    clocks and dark mode

---
+++ evergreen_newtab/evergreen.js
@@ -0,0 +1,27 @@
+setInterval(datetime, 500);
+
+function datetime() {
+    var date = new Date();
+    var h = date.getHours(); // 0 - 23
+    var m = date.getMinutes(); // 0 - 59
+    var s = date.getSeconds(); // 0 - 59
+    var session = "AM";
+    if (h === 0) {
+        h = 12;
+    }
+    if (h > 12) {
+        h = h - 12;
+        session = "PM";
+    }
+    // h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+    var time = h + ":" + m + ":" + s + " " + session;
+    $(".clock").html(time);
+    var d = date.getDate();
+    var mo = date.getMonth() + 1;
+    var y = date.getFullYear();
+    var da = `${mo}/${d}/${y}`;
+    $(".date").html(da);
+
+}
