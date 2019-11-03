db87c01a0fb439bc28a55322d0801a8a1d9265dd
Melody <reticivis@gmail.com>
2019-11-02 20:27:27 -0500

    im not even keeping track at this point

--- evergreen_newtab/evergreen.js
+++ evergreen_newtab/evergreen.js
@@ -1,5 +1,7 @@
 var blur = 0;
 var tempunit = "f";
+var timeformat = "12";
+var dateformat = "md";
 
 function round(value, decimals) {
     return Number(Math.round(value + 'e' + decimals) + 'e-' + decimals);
@@ -10,23 +12,38 @@ function datetime() {
     var h = date.getHours(); // 0 - 23
     var m = date.getMinutes(); // 0 - 59
     var s = date.getSeconds(); // 0 - 59
-    var session = "AM";
-    if (h === 0) {
-        h = 12;
+    if (timeformat === "12") {
+        var session = "AM";
+        if (h === 0) {
+            h = 12;
+        }
+        if (h > 12) {
+            h = h - 12;
+            session = "PM";
+        }
+        m = (m < 10) ? "0" + m : m;
+        s = (s < 10) ? "0" + s : s;
+        var time = h + ":" + m + ":" + s + " " + session;
+
+    } else {
+        h = (h < 10) ? "0" + h : h;
+        m = (m < 10) ? "0" + m : m;
+        s = (s < 10) ? "0" + s : s;
+        var time = h + ":" + m + ":" + s;
     }
-    if (h > 12) {
-        h = h - 12;
-        session = "PM";
-    }
+
     // h = (h < 10) ? "0" + h : h;
-    m = (m < 10) ? "0" + m : m;
-    s = (s < 10) ? "0" + s : s;
-    var time = h + ":" + m + ":" + s + " " + session;
+
     $(".clock").html(time);
     var d = date.getDate();
     var mo = date.getMonth() + 1;
     var y = date.getFullYear();
-    var da = `${mo}/${d}/${y}`;
+    if (dateformat === "md") {
+        var da = `${mo}/${d}/${y}`;
+    } else {
+        var da = `${d}/${mo}/${y}`;
+    }
+
     $(".date").html(da);
 
 }
@@ -41,7 +58,7 @@ function weatherRoutine() {
 
 function weather(position) {
     Weather.getCurrentLatLong(position.coords.latitude, position.coords.longitude, function (current) {
-        if (tempunit == "f") {
+        if (tempunit === "f") {
             var weather = Math.round(Weather.kelvinToFahrenheit(current.temperature()));
         } else {
             var weather = Math.round(Weather.kelvinToCelsius(current.temperature()));
@@ -66,13 +83,30 @@ function sliderblur() {
     sblur(this.value);
 }
 
-function unitchange() {
+function tempunithandler() {
     if (this.id === "farradio") {
         tempunit = "f";
     } else {
         tempunit = "c";
     }
     weatherRoutine();
+}
+
+function timeformathandler() {
+    if (this.id === "12radio") {
+        timeformat = "12";
+    } else {
+        timeformat = "24";
+    }
+}
+
+function dateformathandler() {
+    if (this.id === "mdradio") {
+        dateformat = "md";
+    } else {
+        dateformat = "dm";
+    }
+
 }
 
 function sblur(val) {
@@ -88,33 +122,27 @@ function sblur(val) {
 }
 
 function chstorage() {
-    chrome.storage.sync.set({blurval: blur}, function () {
-    });
-    chrome.storage.sync.set({tempunit: tempunit}, function () {
-    });
+    chrome.storage.sync.set({blurval: blur});
+    chrome.storage.sync.set({tempunit: tempunit});
+    chrome.storage.sync.set({timeformat: timeformat});
+    chrome.storage.sync.set({dateformat: dateformat});
 }
 
-$(document).ready(function () {
-    Weather.APIKEY = "5b01b9ed56e3751931257dde5e952fae";
-
-    //popovers
-    $("#weatherpopover").attr("data-content", `<b>test123</b><img src="evergreen128.png"/>`);
-    $("#evergreenpopover").attr("data-content", `<h2><img class="logoimg" src="evergreen128.png"/>Evergreen</h2><h4>New Tab for Chrome</h4><h5>Created by Reticivis</h5>`);
-    $("#timepopover").attr("data-content", `<div id="tpop"></div>`);
-    caleandar(document.getElementById('caltemp'));
-    var caltemp = $("#caltemp").html();
-    $("#datepopover").attr("data-content", `<div id="caltemp">${caltemp}</div>`);
-    $("#caltemp").remove();
-    $('[data-toggle="popover"]').popover({html: true});
+function optionsinit() {
     //blur handler
     var slider = document.getElementById('blurslider');
     slider.addEventListener('input', sliderblur);
-    document.getElementById('farradio').addEventListener('input', unitchange);
-    document.getElementById('celradio').addEventListener('input', unitchange);
     chrome.storage.sync.get(['blurval'], function (result) {
+        if (result["blurval"] == undefined) {
+            result["blurval"] = "0";
+        }
+        blur = result["blurval"];
         sblur(result["blurval"]);
         $("#blurslider").attr("value", result["blurval"])
     });
+    //temperature unit handler
+    document.getElementById('farradio').addEventListener('input', tempunithandler);
+    document.getElementById('celradio').addEventListener('input', tempunithandler);
     chrome.storage.sync.get(['tempunit'], function (result) {
         tempunit = result["tempunit"];
         if (tempunit == undefined) {
@@ -128,6 +156,53 @@ $(document).ready(function () {
         }
         weatherRoutine();
     });
+    //timeformat handler
+    document.getElementById('12radio').addEventListener('input', timeformathandler);
+    document.getElementById('24radio').addEventListener('input', timeformathandler);
+    chrome.storage.sync.get(['timeformat'], function (result) {
+        timeformat = result["timeformat"];
+        if (timeformat == undefined) {
+            timeformat = "12";
+        }
+        //weather routine
+        if (timeformat == "12") {
+            $("#12radio").attr("checked", "checked");
+        } else {
+            $("#24radio").attr("checked", "checked");
+        }
+    });
+    //dateformat handler
+    document.getElementById('mdradio').addEventListener('input', dateformathandler);
+    document.getElementById('dmradio').addEventListener('input', dateformathandler);
+    chrome.storage.sync.get(['dateformat'], function (result) {
+        dateformat = result["dateformat"];
+        if (dateformat == undefined) {
+            dateformat = "md";
+        }
+        //weather routine
+        if (dateformat == "md") {
+            $("#mdradio").attr("checked", "checked");
+        } else {
+            $("#dmradio").attr("checked", "checked");
+        }
+    });
+}
+
+$(document).ready(function () {
+    Weather.APIKEY = "5b01b9ed56e3751931257dde5e952fae";
+
+    //popovers
+    $("#weatherpopover").attr("data-content", `<b>test123</b><img src="evergreen128.png"/>`);
+    $("#evergreenpopover").attr("data-content", `<h2><img class="logoimg" src="evergreen128.png"/>Evergreen</h2><h4>New Tab for Chrome</h4><h5>Created by Reticivis</h5>`);
+    $("#timepopover").attr("data-content", `<div id="tpop"></div>`);
+    //calendar
+    caleandar(document.getElementById('caltemp'));
+    var caltemp = $("#caltemp").html();
+    $("#datepopover").attr("data-content", `<div id="caltemp">${caltemp}</div>`);
+    $("#caltemp").remove();
+
+    $('[data-toggle="popover"]').popover({html: true});
+    optionsinit();
     setInterval(regularinterval, 100);
     setInterval(chstorage, 3000);
 
